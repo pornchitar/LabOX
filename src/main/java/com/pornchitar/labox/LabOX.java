8e0120603313ec7f3b39d3f6bb83b945ea88e4c1
Pornchitar Sarsai <kream21574@gmail.com>
2023-07-08 21:08:37 +0700

    show

--- src/main/java/com/pornchitar/labox/LabOX.java
+++ src/main/java/com/pornchitar/labox/LabOX.java
@@ -132,6 +132,18 @@ public class LabOX {
         return false;
     }
 
+    public void show() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
     public static void main(String[] args) {
         LabOX project = new  LabOX();
         Scanner kb = new Scanner(System.in);
