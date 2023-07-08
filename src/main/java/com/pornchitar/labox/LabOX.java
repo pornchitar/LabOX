aedd40304086ad7806d8a53c0cec33d145cbe57f
Pornchitar Sarsai <kream21574@gmail.com>
2023-07-08 20:27:40 +0700

    startGame

--- src/main/java/com/pornchitar/labox/LabOX.java
+++ src/main/java/com/pornchitar/labox/LabOX.java
@@ -4,13 +4,43 @@
 
 package com.pornchitar.labox;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ASUS
  */
 public class LabOX {
 
+    Scanner kb = new Scanner(System.in);
+    public int row;
+    public int col;
+    public String[][] board = {{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
+    public boolean play = false;
+    public String turn;
+    public String start;
+    public boolean end;
+    
+    public void startGame() {
+        System.out.println("OX GAME");
+        System.out.print("Start Game??? (y/n): ");
+        start = kb.nextLine().toLowerCase();
+        while (!start.equals("n") && !start.equals("y")) {
+            System.out.print("Start Game (y/n): ");
+            start = kb.nextLine().toLowerCase();
+        }
+        if (start.equals("n")) {
+            play = false;
+        } else {
+            play = true;
+        }
+
+    }
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+         LabOX project = new  LabOX();
+       
     }
 }
